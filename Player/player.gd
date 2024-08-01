38e8e9599148eebc4f938ac5d3b8887d471c2462
Vadim <vadim2505021@gmail.com>
2024-08-01 20:05:01 +0300

    Добавил тестовую модель игрока без анимации
    *Убрал флипы в игроке

--- Player/player.gd
+++ Player/player.gd
@@ -21,16 +21,16 @@ func _physics_process(delta):
 	velocity = Vector2.ZERO
 	if Input.is_action_pressed("right"):
 		velocity.x += SPEED
-		$Sprite2D.flip_h = false
+		#$Sprite2D.flip_h = false
 	if Input.is_action_pressed("left"):
 		velocity.x -= SPEED
-		$Sprite2D.flip_h = true
+		#$Sprite2D.flip_h = true
 	if Input.is_action_pressed("up"):
 		velocity.y -= SPEED
-		$Sprite2D.flip_v = false
+		#$Sprite2D.flip_v = false
 	if Input.is_action_pressed("down"):
 		velocity.y += SPEED
-		$Sprite2D.flip_v = true
+		#$Sprite2D.flip_v = true
 	move_and_slide()
 	$Camera2D.position = Vector2(0,0)
 	var mouse_pos = get_global_mouse_position()
